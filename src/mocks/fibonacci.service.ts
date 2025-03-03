2479eb8113c38769fbe94f5a733880cb2eb8958c
Antoine <123865300+cassaga@users.noreply.github.com>
2025-03-03 15:56:41 +0100

    Update fibonacci.service.ts

--- src/mocks/fibonacci.service.ts
+++ src/mocks/fibonacci.service.ts
@@ -9,7 +9,7 @@ export class FibonacciService {
 
   /**
    * This observable will emit an operation as a string every seconds
-   * Example : "1 + 1 =", "2 + 1 =", "3 + 2 =", etc
+   * Example : "1 + 1 =", "2 + 1 =", "3 + 2 =", etce e
    */
   static getOperations(): Observable<string> {
     console.log(`Start gettings operations one by one`);
